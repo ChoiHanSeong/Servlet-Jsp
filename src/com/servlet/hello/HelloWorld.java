de7d5e2164d2d388cab49afb9cec330aed8fc95f
XPS9360 <chs0325@gmail.com>
2019-02-10 22:00:55 +0900

    .

--- src/com/servlet/hello/HelloWorld.java
+++ src/com/servlet/hello/HelloWorld.java
@@ -10,10 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // 어노테이션을 이용해서 여러개의 url 패턴을 매핑할수 있음..
-@WebServlet(name="HelloWorld", urlPatterns= {"/hello", "helloworld", "hello/world"})
+@WebServlet(
+	name = "MyOwnServlet",
+	description = "This is my first annotated servlet",
+	urlPatterns = {"/hello", "/hello2", "/hello3"}
+)
 public class HelloWorld extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("HelloWorld class Call...");
+		
 		String value = req.getParameter("name");
 		
 		PrintWriter out = resp.getWriter();
